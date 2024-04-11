bb0adba3cfb00b4f83b4c96d858ad0123173c148
Claudio Cambra <developer@claudiocambra.com>
2024-04-11 19:37:20 +0800

    Add more invalid input tests for picture metadata block

--- Tests/FLACMetadataKitTests/Metadata/Blocks/FLACPictureMetadataBlockTests.swift
+++ Tests/FLACMetadataKitTests/Metadata/Blocks/FLACPictureMetadataBlockTests.swift
@@ -73,4 +73,88 @@ final class FLACPictureMetadataBlockTests: XCTestCase {
             XCTFail("Initialization should not succeed with invalid data.")
         }
     }
+
+    func testInitializationWithInvalidPicData() {
+        let type = FLACPictureMetadataBlock.PictureType.frontCover.rawValue
+        let mimeType = "image/jpeg"
+        let description = "A description"
+        let imageData = Data(repeating: 0xFF, count: 7) // Invalid image data
+
+        var data = Data()
+        data.append(contentsOf: withUnsafeBytes(of: type.bigEndian, Array.init))
+        data.append(contentsOf: withUnsafeBytes(
+            of: UInt32(mimeType.utf8.count).bigEndian, Array.init)
+        )
+        data.append(mimeType.data(using: .utf8)!)
+        data.append(contentsOf: withUnsafeBytes(
+            of: UInt32(description.utf8.count).bigEndian, Array.init)
+        )
+        data.append(description.data(using: .utf8)!)
+        // Adding mock values for width, height, color depth, and colors used
+        data.append(contentsOf: [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 24, 0, 0, 0, 0])
+        // Adding incorrect length of the image data and the image data itself
+        data.append(contentsOf: withUnsafeBytes(of: UInt32(10).bigEndian, Array.init))
+        data.append(imageData)
+
+        let headerData = mockHeader(isLast: false, type: .picture, dataSize: UInt32(data.count))
+        do {
+            let header = try FLACMetadataBlockHeader(bytes: headerData)
+            XCTAssertThrowsError(try FLACPictureMetadataBlock(bytes: data, header: header))
+        } catch {
+            XCTFail("Failed with error: \(error)")
+        }
+    }
+
+    func testInitializationWithInvalidDescription() {
+        let type = FLACPictureMetadataBlock.PictureType.frontCover.rawValue
+        let mimeType = "image/jpeg"
+        let description = "A description"
+
+        var data = Data()
+        data.append(contentsOf: withUnsafeBytes(of: type.bigEndian, Array.init))
+        data.append(contentsOf: withUnsafeBytes(
+            of: UInt32(mimeType.utf8.count).bigEndian, Array.init)
+        )
+        data.append(mimeType.data(using: .utf8)!)
+        data.append(contentsOf: withUnsafeBytes( // Bad description count
+            of: UInt32(description.utf8.count - 1).bigEndian, Array.init)
+        )
+        data.append(description.data(using: .utf8)!)
+        // Adding mock values for width, height, color depth, and colors used
+        data.append(contentsOf: [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 24, 0, 0, 0, 0])
+        data.append(contentsOf: withUnsafeBytes(of: UInt32(0).bigEndian, Array.init))
+
+        let headerData = mockHeader(isLast: false, type: .picture, dataSize: UInt32(data.count))
+        do {
+            let header = try FLACMetadataBlockHeader(bytes: headerData)
+            XCTAssertThrowsError(try FLACPictureMetadataBlock(bytes: data, header: header))
+        } catch {
+            XCTFail("Failed with error: \(error)")
+        }
+    }
+
+    func testInitializationWithInvalidMimetype() {
+        let type = FLACPictureMetadataBlock.PictureType.frontCover.rawValue
+        let mimeType = "image/jpeg"
+
+        var data = Data()
+        data.append(contentsOf: withUnsafeBytes(of: type.bigEndian, Array.init))
+        data.append(contentsOf: withUnsafeBytes( // Bad mimetype count
+            of: UInt32(mimeType.utf8.count - 1).bigEndian, Array.init)
+        )
+        data.append(mimeType.data(using: .utf8)!)
+        data.append(contentsOf: withUnsafeBytes(of: UInt32(0).bigEndian, Array.init)) // Description
+        data.append(description.data(using: .utf8)!)
+        // Adding mock values for width, height, color depth, and colors used
+        data.append(contentsOf: [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 24, 0, 0, 0, 0])
+        data.append(contentsOf: withUnsafeBytes(of: UInt32(0).bigEndian, Array.init))
+
+        let headerData = mockHeader(isLast: false, type: .picture, dataSize: UInt32(data.count))
+        do {
+            let header = try FLACMetadataBlockHeader(bytes: headerData)
+            XCTAssertThrowsError(try FLACPictureMetadataBlock(bytes: data, header: header))
+        } catch {
+            XCTFail("Failed with error: \(error)")
+        }
+    }
 }
